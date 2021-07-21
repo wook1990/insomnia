015bc284619e83448a5807bcbd4ff4f0c45a35de
edward_oh <wook1990@gmail.com>
2021-07-21 17:06:35 +0900

    AEDA START

--- 02. STUDY/[2021]/05.A_EDA/01.Concept.py
+++ 02. STUDY/[2021]/05.A_EDA/01.Concept.py
@@ -1,8 +1,16 @@
 import pandas
 import numpy
 
+PATH = "C:/Users/WAI/OneDrive/03.WORKSPACE/00.DATA/99.KAGGLE/insurance/14. Learning from Imbalaced Insurance Data/"
+DNAME = "aug_train.csv"
+
+train_df = pandas.read_csv(PATH + "aug_train.csv", index_col=False)
 
 # 전체 데이터 셋 요약 정보
+print("{0} 은 {1} 건의 데이터와 {2} 개의 컬럼으로 구성되어 있습니다.".format(DNAME.split(".")[0], train_df.shape[0], train_df.shape[1]))
+type(train_df.info())
+pandas.DataFrame(train_df.info())
+train_df.shape[0]
 
 # 변수 데이터 타입 확인
 # 연속형, 범주형 변수 구분
@@ -11,4 +19,5 @@ import numpy
 
 
 
-
+import inspect
+inspect.getsource()
